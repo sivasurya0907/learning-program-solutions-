98d9db7057c8d6c6cf7253062982d36f50382b8a
sivasurya0907 <118263387+sivasurya0907@users.noreply.github.com>
2025-07-06 20:17:36 +0530

    Delete Week3/Week3 directory

--- Week3/Week3/Spring Core and Maven/Ex1 Configure/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void recordBorrow(String title) {
-        System.out.println("Book \"" + title + "\" borrowed. Logging transaction...");
-    }
-}
--- Week3/Week3/Spring Core and Maven/Ex4_Configure_Maven/librarymanagement_3/src/main/java/com/createmaven/Main.java
+++
@@ -1,7 +0,0 @@
-package com.createmaven;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
